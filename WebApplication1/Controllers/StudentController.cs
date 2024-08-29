743d0f3e3d9261facae45365eb0acd2ec66c44e4
Bala <bala.boscosoft@outlook.com>
2024-08-29 22:48:10 +0530

    chnages

---
+++ WebApplication1/Controllers/StudentController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebApplication1.Controllers
+{
+    public class StudentController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult StudentDetails()
+        {
+            return View("studentView");
+        } 
+        public IActionResult AddStudent()
+        {
+            return View();
+        }
+    }
+
+}
